b0a3b2239eddc933755c6b21f58a1ab9068cc802
Zakaria <zakaria.mestour-etu@etu.univh2c.ma>
2025-10-20 01:01:04 +0100

    inventory-service

--- inventory-service/src/main/java/poo/inventoryservice/InventoryServiceApplication.java
+++ inventory-service/src/main/java/poo/inventoryservice/InventoryServiceApplication.java
@@ -1,7 +1,13 @@
 package poo.inventoryservice;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import poo.inventoryservice.entities.Product;
+import poo.inventoryservice.repositories.ProductRepository;
+
+import java.util.UUID;
 
 @SpringBootApplication
 public class InventoryServiceApplication {
@@ -9,5 +15,31 @@ public class InventoryServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(InventoryServiceApplication.class, args);
     }
+    @Bean
+    CommandLineRunner commandLineRunner(ProductRepository productRepository) {
+        return args -> {
+            productRepository.save(Product.builder()
+                            .id(UUID.randomUUID().toString())
+                            .name("computer")
+                            .price(10000.0)
+                            .quantity(10)
 
+                            .build());
+            productRepository.save(Product.builder()
+                    .id(UUID.randomUUID().toString())
+                    .name("printer")
+                    .price(1999.0)
+                    .quantity(150)
+
+                    .build());
+            productRepository.save(Product.builder()
+                    .id(UUID.randomUUID().toString())
+                    .name("screen")
+                    .price(9994.0)
+                    .quantity(50)
+
+                    .build());
+        };
+
+    }
 }
