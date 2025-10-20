57bf94390a1cc93abd84303e56295a39c18f12c3
Zakaria <zakaria.mestour-etu@etu.univh2c.ma>
2025-10-20 04:32:40 +0100

    gateway dynamic configuration

--- gateway-service/src/main/java/poo/gatewayservice/GatewayServiceApplication.java
+++ gateway-service/src/main/java/poo/gatewayservice/GatewayServiceApplication.java
@@ -1,7 +1,12 @@
 package poo.gatewayservice;
 
+import com.netflix.discovery.DiscoveryClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayServiceApplication {
@@ -10,4 +15,10 @@ public class GatewayServiceApplication {
 		SpringApplication.run(GatewayServiceApplication.class, args);
 	}
 
+	@Bean
+	DiscoveryClientRouteDefinitionLocator discoveryClientRouteDefinitionLocator(
+			ReactiveDiscoveryClient rdc, DiscoveryLocatorProperties dlp
+			) {
+		return new DiscoveryClientRouteDefinitionLocator(rdc, dlp);
+	}
 }
